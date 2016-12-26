f86e93fa53b27bd842badec7fe2af7d422f564e5
Raldo CHEA <raldo.chea@gmail.com>
2016-12-26 16:37:12 +0100

    Import developer command

---
+++ src/AppBundle/Command/ImportDeveloperCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+
+class ImportDeveloperCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:developer:import')
+            ->setDescription('Import d\'un developpeur')
+            ->addArgument('username', InputArgument::REQUIRED, 'Username du développeur')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getContainer();
+        $developer = $container->get('app.developer.service')->getDeveloperByUsername($input->getArgument('username'));
+
+        $container->get('app.developer.repository')->save($developer);
+    }
+}
