e27219debed92ba91541419af477f5848af34b80
Raldo CHEA <raldo.chea@gmail.com>
2016-12-26 16:36:59 +0100

    User controller

---
+++ src/AppBundle/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class UserController extends Controller
+{
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        return $this->render(new Response('ok'));
+    }
+}
