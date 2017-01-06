8622f4c15229a277ec0bc9cb9741a12b5499b59b
Raldo CHEA <raldo.chea@gmail.com>
2017-01-06 10:28:18 +0100

    Import classes useless

--- src/AppBundle/Command/ImportAllProjectsCommand.php
+++ src/AppBundle/Command/ImportAllProjectsCommand.php
@@ -2,8 +2,6 @@
 
 namespace AppBundle\Command;
 
-use AppBundle\Entity\Group;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
