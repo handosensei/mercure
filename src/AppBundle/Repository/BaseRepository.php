23b2ea4ce8cc5cc7f216ef9e358ea1c37b41d6bb
Raldo CHEA <raldo.chea@gmail.com>
2017-03-10 23:09:48 +0100

    Request to find merge request opened

--- src/AppBundle/Repository/BaseRepository.php
+++ src/AppBundle/Repository/BaseRepository.php
@@ -2,10 +2,27 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\EntityRepository;
 
 abstract class BaseRepository extends EntityRepository
 {
+    /**
+     * @param $data
+     * @return null
+     */
+    public function remove($data)
+    {
+        if (empty($data) ) {
+            return null;
+        }
+
+        foreach ($data as $object) {
+            $this->_em->remove($object);
+        }
+        $this->_em->flush();
+    }
+
     /**
      * @param mixed $data
      * @return null|mixed
@@ -46,6 +63,22 @@ abstract class BaseRepository extends EntityRepository
             $result[] = $object;
         }
         $this->_em->flush();
+
+        return $result;
+    }
+
+    /**
+     * @param array $data
+     * @param string $field
+     * @return array
+     */
+    public function parseByKey($data, $field = 'id')
+    {
+        $method = sprintf('get%s', ucfirst($field));
+        $result = [];
+        foreach ($data as $object) {
+            $result[$object->$method()] = $object;
+        }
 
         return $result;
     }
--- src/AppBundle/Repository/MergeRequestRepository.php
+++ src/AppBundle/Repository/MergeRequestRepository.php
@@ -2,7 +2,19 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Project;
+
 class MergeRequestRepository extends BaseRepository
 {
-
+    public function findOpened(Project $project)
+    {
+        return $this->createQueryBuilder('m')
+            ->leftJoin('m.project', 'p')
+            ->andWhere('m.status = \'opened\'')
+            ->andWhere('p.id = :projectId')
+            ->setParameter('projectId', $project->getId())
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
