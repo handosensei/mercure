d876ba9b8c916fbd69e150d00cdd87b27b2c8c4d
Raldo CHEA <raldo.chea@gmail.com>
2017-04-08 08:30:44 +0200

    update reinit script

--- bin/reset.sh
+++ bin/reset.sh
@@ -9,4 +9,9 @@ php bin/console cache:clear --env=$1
 php bin/console doctrine:database:drop --force --env=$1
 php bin/console doctrine:database:create --env=$1
 php bin/console doctrine:schema:update --force --env=$1
-php bin/console doctrine:fixtures:load -n --env=$1
+#php bin/console doctrine:fixtures:load -n --env=$1
+php bin/console app:database:synchro --env=$1
+php bin/console app:project:use-mr --env=$1
+php bin/console app:merge-request:stat --env=$1
+
+
